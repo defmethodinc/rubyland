34da5bea09eac2400ac987e509acba01a8fbc03a
Steve Brudz <sbrudz@defmethod.io>
2025-01-02 14:42:57 -0600

    Configure simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,16 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'simplecov'
+require 'simplecov-json'
+SimpleCov.start do
+  enable_coverage :branch
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::JSONFormatter,
+    SimpleCov::Formatter::HTMLFormatter
+  ]
+  add_filter 'phoenix-tests'
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
